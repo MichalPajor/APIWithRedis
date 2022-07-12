c4f964312de32e690b1d7e69b20cc649986d24c1
MichalPajor <michal.pajor1998@gmail.com>
2022-07-12 20:07:38 +0200

    Add auto mapper to program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(options =>
 );
 //Register repository interface
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
